de642288d1ae245373e405f954bf0f41931a2cd3
Win-10 <azimovanvarjon777@gmail.com>
2024-07-16 20:31:28 +0200

    first commit

---
+++ src/main/java/de/telran/timetrackinapp/TimeTrackinAppApplication.java
@@ -0,0 +1,13 @@
+package de.telran.timetrackinapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeTrackinAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeTrackinAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/telran/timetrackinapp/TimeTrackinAppApplicationTests.java
@@ -0,0 +1,13 @@
+package de.telran.timetrackinapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeTrackinAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
