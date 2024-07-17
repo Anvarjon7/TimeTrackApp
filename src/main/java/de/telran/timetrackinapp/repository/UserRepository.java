2d30d10b4c84c442c2b11775e26670a096632f17
Win-10 <azimovanvarjon777@gmail.com>
2024-07-17 07:20:32 +0200

    added task-repository

---
+++ src/main/java/de/telran/timetrackinapp/repository/UserRepository.java
@@ -0,0 +1,27 @@
+package de.telran.timetrackinapp.repository;
+
+import de.telran.timetrackinapp.entity.user.User;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    @EntityGraph(value = "User.withRatings", type = EntityGraph.EntityGraphType.LOAD)
+    @Query("SELECT u FROM User u WHERE u.id = :id")
+    Optional<User> findByIdWithRatings(Long id);
+
+    Optional<User> findByIdAndEmail(Long id, String email);
+
+    Optional<User> findById(Long id);
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+    boolean existsById(Long id);
+}
