3c8fe5f55aa99737f65445006fcd5c5facdc4071
Win-10 <azimovanvarjon777@gmail.com>
2024-07-24 11:43:17 +0200

    added front-end codes

---
+++ timetrackfront/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Appbar from './components/Appbar';
+import UserTextFields from './components/UserTextFields';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Appbar/>
+      <UserTextFields/>
+    </div>
+  );
+}
+
+export default App;
